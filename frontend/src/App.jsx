44a187dfee8ee1ab7401de7e22e8ed67423f9fde
Jesus Campos <jesus.campos123@icloud.com>
2023-07-30 01:25:28 -0500

    frontend: add react-router-dom inital setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home page</h1>} />
+        <Route path="*" element={<h1>404 Not found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
