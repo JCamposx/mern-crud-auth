e4aa9bf495dd538c39f9572992356c711d7fb63b
Jesus Campos <jesus.campos123@icloud.com>
2023-07-31 02:48:35 -0500

    frontend: add error messages on register page

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -2,8 +2,11 @@ import { useDispatch, useSelector } from "react-redux";
 
 import registerRequest from "../api/auth/register.js";
 import { signup as signupReducer } from "../features/user/userSlice.js";
+import { useState } from "react";
 
 const useAuth = () => {
+  const [errors, setErrors] = useState(null);
+
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
   const dispatch = useDispatch();
 
@@ -12,10 +15,12 @@ const useAuth = () => {
 
     if (response.success) {
       dispatch(signupReducer(response.data));
+    } else {
+      setErrors(response.errors);
     }
   };
 
-  return { isAuthenticated, signup };
+  return { isAuthenticated, signup, errors };
 };
 
 export default useAuth;
--- frontend/src/pages/auth/Register.jsx
+++ frontend/src/pages/auth/Register.jsx
@@ -6,9 +6,13 @@ import useAuth from "../../hooks/useAuth.js";
 import { routes } from "../../utils/routes.js";
 
 const Register = () => {
-  const { isAuthenticated, signup } = useAuth();
+  const { isAuthenticated, signup, errors: registerErrors } = useAuth();
 
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const navigate = useNavigate();
 
@@ -24,6 +28,16 @@ const Register = () => {
 
   return (
     <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+      {registerErrors && (
+        <div className="bg-red-500 p-2 text-white rounded-md">
+          {registerErrors.map((error, index) => (
+            <p key={index}>
+              {`- ${error}`}
+            </p>
+          ))}
+        </div>
+      )}
+
       <form onSubmit={onSubmit}>
         <input
           type="text"
@@ -31,18 +45,27 @@ const Register = () => {
           {...register("username", { required: true })}
           className="w-full bg-zinc-700 text-white px-4 py-2 mt-4 rounded-md"
         />
+        {errors.username && (
+          <p className="text-red-500">Username is required</p>
+        )}
+
         <input
           type="text"
           placeholder="Email"
           {...register("email", { required: true })}
           className="w-full bg-zinc-700 text-white px-4 py-2 mt-4 rounded-md"
         />
+        {errors.email && <p className="text-red-500">Email is required</p>}
+
         <input
           type="password"
           placeholder="Password"
           {...register("password", { required: true })}
           className="w-full bg-zinc-700 text-white px-4 py-2 mt-4 rounded-md"
         />
+        {errors.password && (
+          <p className="text-red-500">Password is required</p>
+        )}
 
         <button type="submit" className="mt-4">
           Register
