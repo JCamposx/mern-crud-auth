2bcaf709c5349294f25e498a53e7333d43d08004
Jesus Campos <jesus.campos123@icloud.com>
2023-07-31 01:43:58 -0500

    frontend: add redux setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
+import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { store } from "./utils/store.js";
 
 import Register from "./pages/auth/Register.jsx";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<h1>Home page</h1>} />
-        <Route path="*" element={<h1>404 Not found</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<h1>Home page</h1>} />
+          <Route path="*" element={<h1>404 Not found</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
